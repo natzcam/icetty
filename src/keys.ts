454f467c8bf22b95c07cec40e2d59595a18e40e6
Nathaniel Camomot <nathaniel.camomot@legalmatch.com>
2018-12-28 23:52:28 +0800

    ci

---
+++ src/keys.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID
+  },
+  googleDevice: {
+    clientId: process.env.GOOGLE_DEVICE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_DEVICE_CLIENT_SECRET
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID
+  }
+};
